c0b0aa9c70034b09f7c7dc011b6e838d77bcc563
Dave Creelman <creelman.david@gmail.com>
2025-10-28 15:40:20 +1100

    Check the error returned from the migrate function.

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -15,7 +15,10 @@ func main() {
 	// Creating this one constraint seems to initialise both the steps and ingredients
 	// constraint.
 	log.Println("About to migrate...")
-	initialisers.DB.Migrator().CreateConstraint(&models.Recipe{}, "fk_recipe_recipe_steps")
+	err := initialisers.DB.Migrator().CreateConstraint(&models.Recipe{}, "fk_recipe_recipe_steps")
+	if err != nil {
+		log.Fatal("Unable to setup constraints!!!")
+	}
 	
 	initialisers.DB.AutoMigrate(&models.RecipeStep{})	
 	initialisers.DB.AutoMigrate(&models.RecipeIngredient{})
