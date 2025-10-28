09108eef26ebe4e2488bc1b471be272352f6e585
Dave Creelman <creelman.david@gmail.com>
2025-10-28 15:51:11 +1100

    Fix up the ordering of the DB creation steps.. Do the constraint after the tables have been created.

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -15,12 +15,14 @@ func main() {
 	// Creating this one constraint seems to initialise both the steps and ingredients
 	// constraint.
 	log.Println("About to migrate...")
+
+	initialisers.DB.AutoMigrate(&models.Recipe{})	
+	initialisers.DB.AutoMigrate(&models.RecipeStep{})	
+	initialisers.DB.AutoMigrate(&models.RecipeIngredient{})
+
 	err := initialisers.DB.Migrator().CreateConstraint(&models.Recipe{}, "fk_recipe_recipe_steps")
 	if err != nil {
 		log.Fatal("Unable to setup constraints!!!", err)
 	}
 	
-	initialisers.DB.AutoMigrate(&models.RecipeStep{})	
-	initialisers.DB.AutoMigrate(&models.RecipeIngredient{})
-	initialisers.DB.AutoMigrate(&models.Recipe{})	
 }
